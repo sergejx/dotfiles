0ebc58584c17c12be7562fe2d1e3875998f8a81b
Sergej Chodarev <sergejx@centrum.sk>
2010-12-29 15:20:13 +0100

    Remove unused scripts

--- bin/server.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# Start web server in current directory
-# (may be useful for previewing static web site)
-
-from BaseHTTPServer import *
-from SimpleHTTPServer import *
-
-httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
-httpd.serve_forever()
