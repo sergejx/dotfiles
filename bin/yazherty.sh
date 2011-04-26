0bc5f093d791c94f5ff2b83a45046b7dc8a3d26a
Sergej Chodarev <sergejx@centrum.sk>
2011-04-26 14:42:09 +0200

    Update yazherty script

--- bin/yazherty.sh
+++ bin/yazherty.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # Swap YA and ZHE in russian phonetic keyboard
-patch -p0 -b <<EOD
+cd /usr/share/X11/xkb/symbols/
+patch -p6 -b <<EOD
 --- /usr/share/X11/xkb/symbols/ru	2008-04-15 04:53:53.000000000 +0200
 +++ ru-yazherty	2008-09-16 11:22:16.000000000 +0200
-@@ -154 +154 @@
+@@ -180 +180 @@
 -    key	<LatW> {	[     Cyrillic_ve,     Cyrillic_VE	]	};
 +    key	<LatW> {	[     Cyrillic_zhe,     Cyrillic_ZHE	]	};
-@@ -159 +159 @@
+@@ -185 +185 @@
 -    key	<LatV> {	[    Cyrillic_zhe,    Cyrillic_ZHE	]	};
 +    key	<LatV> {	[    Cyrillic_ve,    Cyrillic_VE	]	};
 EOD
