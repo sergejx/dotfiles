eaf4d6ec495a272930fcba54126bbe3efcf9cf96
Sergej Chodarev <sergejx@sergejx.net>
2019-01-06 19:07:34 +0100

    Use RCM for managing dotfiles + some cleanup

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-dotfiles=('atom' 'gitconfig' 'vim' 'vimrc' 'zimrc' 'zlogin' 'zprofile' 'zshrc')
-dotfiles_dir="$HOME/Code/dotfiles"
-
-for item in "${dotfiles[@]}"; do
-    echo "\n$dotfiles_dir/$item -> $HOME/.$item"
-    ln -Tis "$dotfiles_dir/$item" "$HOME/.$item"
-done
