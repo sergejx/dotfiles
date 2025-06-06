1ba2cf3632e38255676bc8e57ede2692499ee342
Sergej Chodarev <sergejx@sergejx.net>
2025-06-06 15:40:27 +0200

    Update nvim config

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,9 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.maplocalleader = ";"
-vim.opt.spelllang = "sk,en"
+-- vim.g.maplocalleader = ";"
 vim.g.autoformat = false
 vim.g.lazyvim_markdown_lint = false
+vim.opt.spelllang = {"sk","en"}
 vim.opt.wrap = true
 vim.opt.breakindent = true
+vim.opt.clipboard = "" -- Don't sync with system clipboard
+
